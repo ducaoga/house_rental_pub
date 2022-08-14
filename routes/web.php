3e5894010c8ada394f14d9e860550dfc199033cb
danielcatindoy <dc.cerenimbus@gmail.com>
2022-08-14 17:50:25 +0800

    added pretty route to display route list

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//DC: added here the routes for clearing the cache which can be used when new version of prod was released
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return redirect()->route('/');
+});
